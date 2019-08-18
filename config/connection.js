1fac8755a4945ea3d508759b1200bb8c164ab29d
Deb <49885983+dawarrick@users.noreply.github.com>
2019-08-18 17:36:40 -0400

    Initial load.

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "goColorado3Richmond",
+  password: "yourpasswordhere",
   database: "burgers_db"
 });
 
